5e4e475cc7b3d42d2228f670235a90060c430304
PauloScarano <scaranopaulo@gmail.com>
2026-06-11 21:22:54 -0300

    JWT implementado com sucesso

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,8 +1,8 @@
 import * as Yup from 'yup';
-
 import User from '../models/User.js';
-
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import authConfig from '../../config/auth.js'
 
 class SessionController {
   async store(request, response) {
@@ -50,11 +50,17 @@ class SessionController {
     if (!isPasswordCorrect) {
       emailOrPasswordIncorrect();
     }
+
+    const token = jwt.sign({id: existingUser.id}, authConfig.secret,{
+      expiresIn: authConfig.expiresIn
+    })
+
     return response.status(200).json({
       id: existingUser.id,
       name: existingUser.name,
       email: existingUser.email,
       admin: existingUser.admin,
+      token,
     });
   }
 }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: 'ba5a82d808eee0b6b4de286045789b88',
+    expiresIn: '7d'
+}
